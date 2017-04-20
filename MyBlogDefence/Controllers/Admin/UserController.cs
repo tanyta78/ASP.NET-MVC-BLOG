4676b5ba6eb610d56175ba20257a0bfe3f6d5951
tanyta78 <tanyta78@gmail.com>
2017-04-20 11:11:13 +0300

    Add admin panel

---
+++ MyBlogDefence/Controllers/Admin/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MyBlogDefence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlogDefence.Controllers.Admin
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: User/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var users = database.Users.ToList();
+
+                var admins = GetAdminUserNames(users, database);
+                ViewBag.Admins = admins;
+
+                return View(users);
+            }
+        }
+        //
+        // GET: User
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        private HashSet<string> GetAdminUserNames(List<ApplicationUser>users, BlogDbContext conext)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(conext));
+
+            var admins = new HashSet<string>();
+
+            foreach (var user in users)
+            {
+                if (userManager.IsInRole(user.Id, "Admin"))
+                {
+                    admins.Add(user.UserName);
+                }
+            }
+
+            return admins;
+        }
+
+        
+    }
+}
