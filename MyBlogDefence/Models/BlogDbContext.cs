f72a09d7be6b1794b131718b27b558c292eaa373
tanyta78 <tanyta78@gmail.com>
2017-04-24 09:22:39 +0300

    CRED Categories

--- MyBlogDefence/Models/BlogDbContext.cs
+++ MyBlogDefence/Models/BlogDbContext.cs
@@ -17,6 +17,8 @@ namespace MyBlogDefence.Models
 
         public virtual IDbSet<Article> Articles { get; set; }
 
+        public virtual IDbSet<Category>Categories { get; set; }
+
         public static BlogDbContext Create()
         {
             return new BlogDbContext();
